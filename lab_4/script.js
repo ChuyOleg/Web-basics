a2c1933cb9791f99a606f4c898f06f836baf660c
Oleh Chui <misterchuy21@gmail.com>
2022-10-27 16:26:49 +0300

    Lab_4.2: complete task №2 for lab №4

--- lab_4/script.js
+++ lab_4/script.js
@@ -8,6 +8,13 @@ const BLACK_COLOR = "black";
 const THIRD_ELEMENT = document.getElementById("education");
 const FOURTH_ELEMENT = document.querySelector(".hobby");
 
+const REDUCE_IMG_SIZE_BUTTON = document.getElementById("reduceImgSize");
+const INCREASE_IMG_SIZE_BUTTON = document.getElementById("increaseImgSize");
+const ADD_IMG_BUTTON = document.getElementById("addImg");
+const REMOVE_IMG_BUTTON = document.getElementById("removeImg");
+const PHOTO_CONTAINER = document.querySelector(".img-container");
+const PHOTO_ELEMENT_COPY = document.querySelector(".VolodumurImg").cloneNode();
+
 const changeColor = element => () => {
     if (element.style.backgroundColor === BLUE_COLOR) {
         element.style.color = BLACK_COLOR;
@@ -18,5 +25,40 @@ const changeColor = element => () => {
     }
 }
 
+const findLastElemInContainer = container => {
+    return container.querySelector("img:last-child");
+}
+
+const reduceImgSize = container => () => {
+    const lastElem = findLastElemInContainer(container);
+    if (lastElem.height > 100) {
+        lastElem.height -= 100;
+        lastElem.width -= 220;
+    }
+}
+
+const increaseImgSize = container => () => {
+    const lastElem = findLastElemInContainer(container);
+    if (lastElem.height < 600) {
+        lastElem.height += 100;
+        lastElem.width += 220;
+    }
+}
+
+const addElement = container => () => {
+    container.appendChild(PHOTO_ELEMENT_COPY.cloneNode());
+}
+
+const removeElement = container => () => {
+    console.log(container)
+    const lastElem = findLastElemInContainer(container);
+    lastElem.remove();
+}
+
 THIRD_ELEMENT.addEventListener("click", changeColor(THIRD_ELEMENT));
 FOURTH_ELEMENT.addEventListener("click", changeColor(FOURTH_ELEMENT));
+
+REDUCE_IMG_SIZE_BUTTON.addEventListener("click", reduceImgSize(PHOTO_CONTAINER));
+INCREASE_IMG_SIZE_BUTTON.addEventListener("click", increaseImgSize(PHOTO_CONTAINER));
+ADD_IMG_BUTTON.addEventListener("click", addElement(PHOTO_CONTAINER));
+REMOVE_IMG_BUTTON.addEventListener("click", removeElement(PHOTO_CONTAINER));
